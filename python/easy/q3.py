6d4129b09773122de7c4e6f3743394fdafd21eb1
ChamanEIqbal <pc19232.mustafa@gmail.com>
2024-11-30 09:31:08 +0500

    q3 py edit

--- python/easy/q3.py
+++ python/easy/q3.py
@@ -1,3 +1,19 @@
+""" ##
+ # In a distant future, on a war-torn planet named Syphara, a group of elite space engineers has been 
+ # tasked with decrypting a message hidden within an ancient, alien artifact. The message, however, is
+ # encoded in reverse. To unlock the secrets of the artifact, they must reverse the alien text without
+ # the use of modern technologies that can reverse strings automatically.
+
+ # As one of the engineers, you must correct an ancient program to reverse the string manually, using your advanced
+ # understanding of algorithms. The fate of Syphara depends on your ability to solve this challenge and retrieve
+ # the precious data hidden within the artifact!
+# 
+
+ ##
+ # SAMPLE INPUT: "tset neila XAE II IV III VI"
+ # SAMPLE OUTPUT: "IV III VI II EAX alien test"
+"""
+
 class SypharaArtifact:
 
 class Engineer:
@@ -15,7 +31,6 @@ class Engineer:
                 temp = str[start]
                 str[start] = str[end]
             str[end] = temp
-
                 start += 1
 
             return ''.join(str)  
